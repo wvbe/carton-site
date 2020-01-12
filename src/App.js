be5ed1f50b58c8fa00234af2fa4beff2c2c12b35
wvbe <wybe@x-54.com>
2020-01-12 12:03:57 +0100

    [wip2]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import MarkdownScreen from './MarkdownScreen';
+import React, { Fragment, useState, useCallback } from 'react';
+import Markdown from './Markdown';
+import rm from './blog/test.md';
 
+const states = {
+	IDLE: 0,
+	LOADING: 1,
+	DONE: 2,
+	ERROR: 3
+};
+
+const markdownFiles = [rm];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-	  	<MarkdownScreen markdownString={'# Nerf'}/>
-      </header>
+	const [activeFile, setActiveFile] = useState(null);
+	const [activeContent, setActiveContent] = useState(null);
+	const [status, setStatus] = useState(states.IDLE);
 
-    </div>
-  );
+	const activateMd = useCallback(async (md) => {
+		setActiveFile(md);
+		setStatus(states.LOADING);
+		const content = await fetch(md).then(r => r.text());
+		setActiveContent(content);
+		setStatus(states.DONE);
+	});
+	return <div style={{ maxWidth: '800px', margin: '0 auto'}}>
+		<ol>
+			{markdownFiles.map(f => <li key={f}><a href="#" onClick={() => activateMd(f)}>{f}</a></li>)}
+		</ol>
+		{activeContent && <hr />}
+		{activeContent && <Markdown markdownString={activeContent} />}
+
+	</div>
 }
 
 export default App;
---
+++ src/Markdown.js
@@ -0,0 +1,82 @@
+import React, { Fragment, useState, useMemo, useEffect } from 'react';
+import unified from 'unified';
+import parse from 'remark-parse';
+
+function useRemarkAst(markdownString = '') {
+	const [ast, setAst] = useState({});
+
+	useEffect(
+		() => {
+			unified()
+				.use(parse)
+				.use(function parser(options) {
+					this.Compiler = (remarkAstNode) => {
+						setAst(remarkAstNode);
+					};
+				})
+				.processSync(markdownString);
+		},
+		[markdownString]
+	);
+
+	return ast;
+}
+
+
+const loggedTypes = [];
+function MarkdownNodes({ nodes }) {
+	return (nodes || []).map((node, i) => <MarkdownNode key={i} node={node} />)
+}
+
+function MarkdownNode({ node }) {
+	if (node.type === 'text') {
+		return node.value;
+	}
+
+	if (node.type === 'paragraph') {
+		return <p><MarkdownNodes nodes={node.children} /></p>;
+	}
+	if (node.type === 'heading') {
+		return React.createElement('h' + node.depth, [], <MarkdownNodes nodes={node.children} />);
+	}
+	if (node.type === 'emphasis') {
+		return <em><MarkdownNodes nodes={node.children} /></em>;
+	}
+	if (node.type === 'strong') {
+		return <strong><MarkdownNodes nodes={node.children} /></strong>;
+	}
+	if (node.type === 'inlineCode') {
+		return <code>{node.value}</code>;
+	}
+	if (node.type === 'code') {
+		return <pre>{node.value}</pre>;
+	}
+	if (node.type === 'link') {
+		return <a href={node.url} target={node.url.startsWith('http') ? '_blank' : '_self'}><MarkdownNodes nodes={node.children} /></a>;
+	}
+	if (node.type === 'list') {
+		return node.ordered ? <ol><MarkdownNodes nodes={node.children} /></ol> : <ul><MarkdownNodes nodes={node.children} /></ul>;
+	}
+	if (node.type === 'listItem') {
+		return <li><MarkdownNodes nodes={node.children} /></li>;
+	}
+	if (node.type === 'thematicBreak') {
+		return <hr />
+	}
+
+	if (!loggedTypes.includes(node.type)) {
+		console.log(node.type, node);
+		loggedTypes.push(node.type);
+	}
+
+	return <MarkdownNodes nodes={node.children} />;
+}
+
+
+export default function MarkdownScreen({ markdownString }) {
+	const ast = useRemarkAst(markdownString);
+
+	return (
+		<MarkdownNode node={ast} />
+	);
+}
